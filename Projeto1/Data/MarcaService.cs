d3277549c496d684e3b176dfe0aa7e1390c9d5b8
Filipe Sousa <filipe.sousa@fortrevo.com>
2025-06-03 09:56:20 +0100

    Add navigation links for managing brands and implement brand addition and deletion functionality

--- Projeto1/Data/MarcaService.cs
+++ Projeto1/Data/MarcaService.cs
@@ -12,14 +12,34 @@ namespace Projeto1.Data
             _context = context;
         }
 
+        // Obter todas as marcas
         public async Task<List<Marca>> GetMarcasAsync()
         {
             return await _context.Marcas.ToListAsync();
         }
 
+        // Obter uma marca por ID
         public async Task<Marca?> GetMarcaByIdAsync(int id)
         {
             return await _context.Marcas.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        // Adicionar uma nova marca
+        public async Task AdicionarMarcaAsync(Marca marca)
+        {
+            _context.Marcas.Add(marca);
+            await _context.SaveChangesAsync();
+        }
+
+        // Apagar uma marca por ID
+        public async Task ApagarMarcaAsync(int id)
+        {
+            var marca = await _context.Marcas.FindAsync(id);
+            if (marca != null)
+            {
+                _context.Marcas.Remove(marca);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }

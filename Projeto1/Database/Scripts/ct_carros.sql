a6428553dfeebb8b8b1963484cfa5e456391ff5a
Filipe Sousa <filipe.sousa@fortrevo.com>
2025-06-05 15:51:35 +0100

    Update database schema for 'Modelos' and 'Marcas' tables; add image field and additional attributes

--- Projeto1/Database/Scripts/ct_carros.sql
+++ Projeto1/Database/Scripts/ct_carros.sql
@@ -2,11 +2,15 @@ CREATE TABLE "Modelos" (
     "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "Nome" TEXT NOT NULL,
     "MarcaId" INTEGER NOT NULL,
-    "Imagem" BLOB,
+    "Ano" INTEGER NOT NULL,
+    "Categoria" TEXT NOT NULL, -- Hatch, SUV, Sedan, etc.
+    "Transmissao" TEXT NOT NULL, -- Manual, Automática
+    "Combustivel" TEXT NOT NULL, -- Gasolina, Elétrico, Híbrido, etc.
     "Potencia" INTEGER NOT NULL,
     "Binario" INTEGER NOT NULL,
     "Motor" TEXT NOT NULL,
     "Tracao" TEXT NOT NULL,
     "Preco" REAL NOT NULL,
+    "Imagem" BLOB,
     FOREIGN KEY("MarcaId") REFERENCES "Marcas"("Id")
 );
--- Projeto1/Database/Scripts/ct_marcas.sql
+++ Projeto1/Database/Scripts/ct_marcas.sql
@@ -1,1 +1,8 @@
-CREATE TABLE "Marcas" ("Id" INTEGER PRIMARY KEY AUTOINCREMENT, "Nome" TEXT NOT NULL);
+CREATE TABLE "Marcas" (
+    "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Nome" TEXT NOT NULL,
+    "PaisOrigem" TEXT NOT NULL, 
+    "Fundacao" INTEGER, 
+    "Sede" TEXT, 
+    "Imagem" BLOB 
+);
